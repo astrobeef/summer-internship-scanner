2bcc516306fd48019d667112d5d8295c5b9c0f2b
AstroBeef <brian.g.graf@gmail.com>
2025-08-02 10:55:47 -0700

    implemented Honeywell fetch

---
+++ fetch_api_honeywell.py
@@ -0,0 +1,131 @@
+"""
+
+"""
+# first-party
+from __future__ import annotations
+import requests, json, glob, os
+# local
+from constants import (
+    TIMEOUT,
+    HITS_SAVE_DIR,
+    JOBS_SAVE_DIR,
+    Job
+)
+from util_fetch_io import (
+    save_hits,
+    save_jobs,
+    load_objects
+    )
+
+SOURCE = "honeywell"
+
+# Derived from honeywell.har
+ENDPOINT = (
+    "https://ibqbjb.fa.ocs.oraclecloud.com/hcmRestApi/resources/latest/recruitingCEJobRequisitions?onlyData=true&expand=requisitionList.workLocation,requisitionList.otherWorkLocations,requisitionList.secondaryLocations,flexFieldsFacet.values,requisitionList.requisitionFlexFields&finder=findReqs;siteNumber=CX_1,facetsList=LOCATIONS%3BWORK_LOCATIONS%3BWORKPLACE_TYPES%3BTITLES%3BCATEGORIES%3BORGANIZATIONS%3BPOSTING_DATES%3BFLEX_FIELDS,limit=25,keyword=%22intern%22,lastSelectedFacet=TITLES,locationId=300000000469866,selectedTitlesFacet=124,sortBy=RELEVANCY"
+)
+
+# Derived from honeywell.har
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) "
+        "Gecko/20100101 Firefox/141.0"
+    ),
+    "Accept": "application/json, text/plain, */*",
+    "Origin": "https://careers.honeywell.com",
+    "Referer": "https://careers.honeywell.com",
+}
+
+#######
+# I/O #
+#######
+
+def _parse_id(hit) -> str:
+    return hit["Id"]
+
+############
+#  FETCH   #
+############
+
+# NOTE: All the keys referenced were derived from the .har file
+def _fetch_hits(
+    timeout_seconds :int,
+    *,
+    save_local      :bool,
+    verbose         :bool,
+) -> list:
+    try:
+        response = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            timeout=timeout_seconds
+        )
+        response.raise_for_status()
+    except requests.exceptions.Timeout:
+        raise RuntimeError("Honeywell request timed out")
+    except requests.HTTPError as exc:
+        raise RuntimeError(
+            f"Honeywell request failed: {response.status_code} {response.text}"
+        ) from exc
+    data  = response.json()
+    hits  = data["items"][0]["requisitionList"]
+    verbose and print(f"Fetched {len(hits)} hits from Honeywell")
+    if save_local and hits:
+        save_hits(hits, source=SOURCE, id_fn=_parse_id, verbose=verbose)
+    return hits
+
+# NOTE: All the keys referenced were derived from the .har file
+def _parse_jobs_from_hits(
+    hits        :list,
+    *,
+    save_local  :bool,
+    verbose     :bool
+) -> list[Job]:
+    jobs: list[Job] = []
+    for h in hits:
+        job: Job = {
+            "source"        :SOURCE,
+            "id"            :_parse_id(h),
+            "title"         :h["Title"],
+            "url"           :"https://careers.honeywell.com/en/sites/Honeywell/job/" + _parse_id(h),        # Not included, but always configured this way
+            "location"      :h["PrimaryLocation"],
+            "contract_type" :"Intern",
+            "unique_meta"   :{
+                    "post_date"       :h["PostedDate"],
+                    "workplace"         :h["WorkplaceType"]
+                }
+        }
+        jobs.append(job)
+    if save_local:
+        save_jobs(jobs, SOURCE, verbose=verbose)
+    return jobs
+
+###########
+# EXECUTE #
+###########
+
+def parse_jobs_fetch_hits(
+    timeout_seconds :int    = TIMEOUT,
+    *,
+    save_local      :bool   = True,
+    verbose         :bool   = False,
+) -> list[Job]:
+    hits = _fetch_hits(timeout_seconds, save_local=save_local, verbose=verbose)
+    return _parse_jobs_from_hits(hits, save_local=save_local, verbose=verbose)
+
+def parse_jobs_cached_hits(
+    *,
+    save_local  :bool = True,
+    verbose     :bool = False
+) -> list[Job]:
+    hits = load_objects(source=SOURCE, dir=HITS_SAVE_DIR, verbose=verbose)
+    return _parse_jobs_from_hits(hits, save_local=save_local, verbose=verbose)
+
+def load_cached_jobs(
+    *,
+    verbose :bool = False
+) -> list[Job]:
+    return load_objects(source=SOURCE, dir=JOBS_SAVE_DIR, verbose=verbose)
+
+if __name__ == "__main__":
+    for job in parse_jobs_fetch_hits(verbose=True):
+        print(f"{job['title']} | {job['location']} | {job['url']}")
